928f2f893716f43bb5da7869a555393c45fa36ad
talmagemorris <talmagemorris1@gmail.com>
2020-11-24 22:45:30 -0700

    push

---
+++ functions.js
@@ -0,0 +1,23 @@
+
+ function addSum (num1, num2){
+     console.log(num1 + num2)
+ }
+
+ addSum(10, 5)
+
+
+ function largestOfThree(num1, num2, num3) {
+    return (Math.max(num1, num2, num3))
+ }
+
+ console.log(largestOfThree(5, 10, 158900))
+    
+  
+ function randonNumber (num1) {
+     if(num1 % 2 == 0){
+     console.log("even")
+     } else {
+     console.log("odd")
+     }
+ }
+ randonNumber(8)
